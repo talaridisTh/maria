319903fd1ad98d4f0894d3e1f82ce24140748051
ttalaridis <ttalaridis@inscope.gr>
2025-08-28 01:00:53 +0300

    feat: add new question categories and validation rules to config schema

--- config/questions.php
+++ config/questions.php
@@ -11,4 +11,14 @@ return [
     //     'coin_reward' => 10,
     //     'is_active' => true,
     // ],
+    [
+        'question' => 'Ποιο είναι το αγαπημένο μου γλυκό;',
+        'content' => '🍬',
+        'type' => 'message',
+        'options' => null,
+        'correct_answer' => null,
+        'extra_text' => null,
+        'coin_reward' => 10,
+        'is_active' => true,
+    ],
 ];
